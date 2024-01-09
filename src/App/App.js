57412a768f71e3f1273f4ca164e39a6336be00de
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2024-01-09 14:12:59 +0200

    remove: Header

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,10 @@
-import Header from "../Components/Header/Header";
 import MainSection from "../Components/MainSection/MainSection";
 import SideSection from "../Components/SideSection/SideSection";
 import css from './App.module.css'
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
       <div className={css.main}>
         <SideSection />
         <MainSection />
